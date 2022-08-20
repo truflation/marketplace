41e610d37d2b214a090316f1f90630ba50e9d5e7
Joseph C Wang <joequant@gmail.com>
2022-08-20 10:59:09 +0800

    add clients

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.6.6",
+  solidity: {
+    compilers: [
+      {
+	version: "0.6.6"
+      },
+      {
+	version: "0.8.15"
+      }
+    ]
+  }
 };
 
 export default config;

a80b230e7b18e51c9d2c50e9af9057f878e10a6a
Joseph C Wang <joequant@gmail.com>
2022-09-13 19:30:35 +0800

    add operator

---
+++ scripts/deploy-operator.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const TfiOperator = await ethers.getContractFactory("TfiOperator");
+  const tfiOperator =
+    await TfiOperator.deploy('0x3417dd955d4408638870723B9Ad8Aae81953B478',
+			     '0x968E88df55AcAeC002e3d7c2393F9742e40d94b9');
+
+  await tfiOperator.deployed();
+  console.log(`operator deployed to ${tfiOperator.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

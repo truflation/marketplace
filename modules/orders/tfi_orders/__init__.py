268885151dca40b620ed55831e1fa2002d90004b
Joseph C Wang <joequant@gmail.com>
2022-09-18 20:18:18 +0800

    allow for different debug values

--- modules/orders/tfi_orders/__init__.py
+++ modules/orders/tfi_orders/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, request, jsonify
+import logging
 
 app = Flask(__name__)
 
@@ -10,8 +11,12 @@ def hello_world():
 @app.route("/", methods=['POST'])
 def process_order():
     content = request.json
-    app.logger.info(content)
+    app.logger.debug(content)
     return jsonify({})
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
 def create_app():
     return app

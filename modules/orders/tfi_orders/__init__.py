7293e64f9c7a53d66915a31365d97db6bbf8055a
Joseph C Wang <joequant@gmail.com>
2022-09-18 23:02:45 +0800

    checkpoint

--- modules/orders/tfi_orders/__init__.py
+++ modules/orders/tfi_orders/__init__.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 from flask import Flask, request, jsonify
 import logging
+import os
+import requests
+import sys
 
 app = Flask(__name__)
+api_adapter = os.getenv('TRUFLATION_API_HOST', 'http://api-adapter:8081')
 
 @app.route("/hello")
 def hello_world():
@@ -14,9 +18,20 @@ def process_order():
     app.logger.debug(content)
     return jsonify({})
 
-gunicorn_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers = gunicorn_logger.handlers
-app.logger.setLevel(gunicorn_logger.level)
+@app.route("/api-adapter", methods=['POST'])
+def process_api_adapter():
+    content = request.json
+    r = requests.post(api_adapter, json=content)
+    return jsonify(r.json())
+
+if os.getenv('TFI_ORDERS_LOCAL_LOGLEVEL') is None:
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+else:
+    app.logger.setLevel(os.getenv('TFI_ORDERS_LOCAL_LOGLEVEL'))
+
+app.logger.info(f'Connecting to adapter at {api_adapter}')
 
 def create_app():
     return app

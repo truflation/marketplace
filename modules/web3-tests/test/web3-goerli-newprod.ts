7ad857e7c6e47a289bc4a28204f4678e25c85c81
Joseph C Wang <joequant@gmail.com>
2022-09-25 12:39:12 +0800

    add newprod test

---
+++ modules/web3-tests/test/web3-goerli-newprod.ts
@@ -0,0 +1,14 @@
+import { testChain, testInflation } from './web3-tests'
+
+const INFURA_API = process.env.INFURA_API
+const config = {
+  apiAddress: '0x0edBa69e2aE5C668a46360964f8a0b402359F2E0',
+  chainName: 'Goerli',
+  chainId: 5,
+  provider: `wss://goerli.infura.io/ws/v3/${INFURA_API}`,
+  poll: 1000
+}
+
+testInflation(config)
+testChain(config)
+

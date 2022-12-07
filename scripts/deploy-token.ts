942c92a1b0e8c3ef540c0718499d2a7f8175556c
Joseph C Wang <joequant@gmail.com>
2022-12-07 15:27:34 +0800

    add console.log

--- scripts/deploy-token.ts
+++ scripts/deploy-token.ts
@@ -1,12 +1,14 @@
 import { ethers } from 'hardhat'
 
 async function main (): void {
+  const name = 'TruflationToken'
   const Token =
-    await ethers.getContractFactory('TruflationToken')
+    await ethers.getContractFactory(name)
   const token =
     await Token.deploy()
 
   await token.deployed()
+  console.log(`${name} deployed to ${token.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere

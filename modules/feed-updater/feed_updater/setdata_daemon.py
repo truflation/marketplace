a67b4defb2032ef265525d21d20fccbc45272985
Joseph C Wang <joequant@gmail.com>
2024-05-11 11:13:17 +0800

    trap file not found

--- modules/feed-updater/feed_updater/setdata_daemon.py
+++ modules/feed-updater/feed_updater/setdata_daemon.py
@@ -72,7 +72,10 @@ class RoundsData:
     def __init__(self, file_path):
         self.rounds = {}
         self.file_path = file_path
-        self.load()
+        try:
+            self.load()
+        except FileNotFoundError:
+            pass
     def load(self):
         with open(self.file_path, 'r',
                   encoding='utf-8') as file:

ee8b346397c58749940c3b90391e667740f4edeb
Joseph C Wang <joequant@gmail.com>
2024-05-11 09:02:56 +0800

    add lock

--- modules/feed-updater/feed_updater/setdata_daemon.py
+++ modules/feed-updater/feed_updater/setdata_daemon.py
@@ -68,6 +68,8 @@ contract = web3.eth.contract(
 )
 chain_id = web3.eth.chain_id
 
+lock = asyncio.Lock()
+
 @app.route('/send-data', methods=['POST'])
 async def handle_send_data(request):
     """
@@ -79,32 +81,30 @@ Handle send data
             return json({'error': 'Invalid JSON format'}, status=400)
         ic(f'Received data: {obj}')
         print('setting')
-        nonce = web3.eth.get_transaction_count(caller)
-        web3.strict_bytes_type_checking = False
-#        dts = int(datetime.datetime.strptime(s, '%Y-%m-%d').timestamp())
-#        nts = int(datetime.datetime.utcnow().timestamp())
-#        roundId = dts
 
-        call_function = contract.functions.setRoundData(
-            bytes(obj['n'], 'utf-8'),
-            obj['r'],
-            obj['v'],
-            obj['s'],
-            obj['u']
-        ).build_transaction({
-            "chainId": chain_id,
-            "gasPrice": web3.eth.gas_price,
-            "from": caller,
-            "nonce": nonce
-        })
-        signed_tx = web3.eth.account.sign_transaction(
-            call_function, private_key=private_key
-        )
-        send_tx = web3.eth.send_raw_transaction(
-            signed_tx.rawTransaction
-        )
-        tx_receipt = web3.eth.wait_for_transaction_receipt(send_tx)
-        ic(f'setting complete txid={tx_receipt.transactionHash.hex()}')
+        async with lock:
+            nonce = web3.eth.get_transaction_count(caller)
+            web3.strict_bytes_type_checking = False
+            call_function = contract.functions.setRoundData(
+                bytes(obj['n'], 'utf-8'),
+                obj['r'],
+                obj['v'],
+                obj['s'],
+                obj['u']
+            ).build_transaction({
+                "chainId": chain_id,
+                "gasPrice": web3.eth.gas_price,
+                "from": caller,
+                "nonce": nonce
+            })
+            signed_tx = web3.eth.account.sign_transaction(
+                call_function, private_key=private_key
+            )
+            send_tx = web3.eth.send_raw_transaction(
+                signed_tx.rawTransaction
+            )
+            tx_receipt = web3.eth.wait_for_transaction_receipt(send_tx)
+            ic(f'setting complete txid={tx_receipt.transactionHash.hex()} nonce={nonce}')
         return json({
             'txid':
             tx_receipt.transactionHash.hex()
